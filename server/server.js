cb6fa1bc91d909523896de4118390150b17a525a
Shivendra Kumar Verma <shanuver.sv@gmail.com>
2021-01-02 21:25:49 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const publicPath = path.join(__dirname, '/../public');
+const app = express();
+
+app.use(express.static('publicPath'));
+
+const PORT = process.env.port || 8000;
+app.listen(PORT, () => {
+	console.log(`Server running on ${PORT}`);
+});
