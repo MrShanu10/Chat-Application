55c6708fabea8f9c66d3762ed401029b825e1b53
Shivendra Kumar Verma <shanuver.sv@gmail.com>
2021-01-03 16:57:15 +0530

    Resolved PORT error

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(publicPath));
 
-const PORT = process.env.port || 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
 	console.log(`Server running on ${PORT}`);
 });
